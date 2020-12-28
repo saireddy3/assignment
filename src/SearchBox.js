233b7be8f22a069d56a4fd6bc4253ac18c778420
Sai Prakash Reddy <saiprakashreddy@vitruvian3d.com>
2020-12-28 21:13:33 +0530

    ''

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBox(props) {
+  return (
+    <div>
+      <h1>Search box</h1>
+    </div>
+  );
+}
+
+export default SearchBox;
---
+++ src/SearchResult.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchResult(props) {
+  return (
+    <div>
+      <h1>Search result</h1>
+    </div>
+  );
+}
+
+export default SearchResult;
